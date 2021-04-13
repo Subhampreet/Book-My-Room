27b87476408768842b05ee127f92a23ebdf14b99
Rupesh Chandra Mohanty <43889891+rupeshmohanty@users.noreply.github.com>
2021-04-14 00:15:17 +0530

    Booking feature changed

--- booking/scripts/booking.php
+++ booking/scripts/booking.php
@@ -6,7 +6,6 @@
     $roomFor = "";
     $name = "";
     $number = "";
-    $aadhar = "";
     $day = 0;
     $month = "";
     $stay = 0;
@@ -53,48 +52,25 @@
     }
 
     // check empty
-    if($roomFor != "" && $name != "" && $number != "" && $day != 0 && $month != "" && $stay != 0 && $roomType != "" && $adults != 0) {
+    if($roomFor != "" && $name != "" && $number != "" && $day != 0 && $month != "" && $stay != 0 && $roomType != "" && $adults != 0) {   
 
-        // check if room in that particular day is not full!
-        $numberOfBookings = 0;
-        $checkAvailability = "SELECT * FROM `bookings` WHERE `day` = '$day' AND `month` = '$month' AND `roomType` = '$roomType'";
-        $checkAvailabilityStatus = mysqli_query($conn,$checkAvailability) or die(mysqli_error($conn));
-        while($checkAvailabilityRow = mysqli_fetch_assoc($checkAvailabilityStatus)) {
-            $numberOfBookings = $numberOfBookings + 1;
-        }
-
-        // check no of bookings for one day
-        $roomLimit = 0;
-        $checkNoBookings = "SELECT * FROM `capacity` WHERE `month` = '$month' AND `roomType` = '$roomType'";
-        $checkNoBookingsStatus = mysqli_query($conn,$checkNoBookings) or die(mysqli_error($conn));
-        $checkNoBookingsRow = mysqli_fetch_assoc($checkNoBookingsStatus);
-        $roomLimit = $checkNoBookingsRow['roomLimit'];
+        // insert into the database!
+        $bookRoom = "INSERT INTO `bookings`(`roomFor`,`name`,`number`,`day`,`month`,`stay`,`roomType`,`adults`,`children`,`bookedAt`) VALUES('$roomFor','$name','$number','$day','$month','$stay','$roomType','$adults','$children','$bookedAt')";
+        $bookRoomStatus = mysqli_query($conn,$bookRoom) or die(mysqli_error($conn));
         
-        if($numberOfBookings < $roomLimit) {
+        if($bookRoomStatus) {
 
-            // insert into the database!
-            $bookRoom = "INSERT INTO `bookings`(`roomFor`,`name`,`number`,`day`,`month`,`stay`,`roomType`,`adults`,`children`,`bookedAt`) VALUES('$roomFor','$name','$number','$day','$month','$stay','$roomType','$adults','$children','$bookedAt')";
-            $bookRoomStatus = mysqli_query($conn,$bookRoom) or die(mysqli_error($conn));
-            
-            if($bookRoomStatus) {
+            // get latest entry id!
+            $getbooking = "SELECT `id` FROM `bookings` ORDER BY `bookedAt` DESC LIMIT 1";
+            $getbookingStatus = mysqli_query($conn,$getbooking) or die(mysqli_error($conn));
+            $getbookingRow = mysqli_fetch_assoc($getbookingStatus);
+            $bookingId = $getbookingRow['id'];
 
-                // get latest entry id!
-                $getbooking = "SELECT `id` FROM `bookings` ORDER BY `bookedAt` DESC LIMIT 1";
-                $getbookingStatus = mysqli_query($conn,$getbooking) or die(mysqli_error($conn));
-                $getbookingRow = mysqli_fetch_assoc($getbookingStatus);
-                $bookingId = $getbookingRow['id'];
-
-                header('Location: ../../checkout/index.php?id='.$bookingId);
-
-            } else {
-
-                header('Location: ../index.php?message=Unable to process your request right now. Please try again later!');
-
-            }
+            header('Location: ../../checkout/index.php?id='.$bookingId);
 
         } else {
 
-            header('Location: ../index.php?message=No rooms available on this date!');
+            header('Location: ../index.php?message=Unable to process your request right now. Please try again later!');
 
         }
 
